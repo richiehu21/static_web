0e8972d16ea14aa6aa50b5e5e7da915997c2588d
Richie Hujaya <richie@Richies-Laptop.local>
2026-03-07 19:00:00 +0700

    fix github cant refresh other page so i use hashrouter instead of reactrouter

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,23 @@ import Navbar from './components/navbar'
 import Homepage from './pages/home'
 import Animepage from './pages/animepage'
 import Moviepage from './pages/movie'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dramapage from './pages/drama'
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     // basename must match your vite.config.js base
-    <BrowserRouter basename="/static_web">
+    <HashRouter>
       <Navbar />
       
       <Routes>
         {/* 'path' stays simple, basename handles the prefix */}
         <Route path="/" element={<Homepage />} />
         <Route path='/movie' element={<Moviepage />} />
-        <Route path="/drama" element={<div>Drama Page</div>} />
+        <Route path="/drama" element={<Dramapage />} />
         <Route path="/anime" element={<Animepage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
---
+++ src/pages/drama.jsx
@@ -0,0 +1,7 @@
+function Home() {
+    return (
+        <div>This is drama page</div>
+    )
+}
+
+export default Home
